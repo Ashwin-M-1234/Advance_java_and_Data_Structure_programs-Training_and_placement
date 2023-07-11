7b67d0488f78d5fe65f9a3b516eb7175dbc9b867
Ashwin .M <114208625+Ashwin-M-1234@users.noreply.github.com>
2023-07-11 19:56:24 +0530

    print 1 to 100.java

---
+++ Day 2/print 1 to 100.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class print_all 
+{
+  public static void main (String args[]) 
+  {
+    for (int i = 1; i<=100; i++) 
+    {
+      System.out.println(i);
+    }
+  }
+}
