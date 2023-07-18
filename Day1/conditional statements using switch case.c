c64e5f23362740e25f0a3c9cff412cd5789359a1
Ashwin .M <114208625+Ashwin-M-1234@users.noreply.github.com>
2023-07-18 09:44:28 +0530

    Created conditional statements using switch case.c

---
+++ Day1/conditional statements using switch case.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+  int day;
+  printf("Type a day : \n");
+  scanf("%d", &day);
+switch (day) {
+  case 1:
+    printf("Monday");
+    break;
+  case 2:
+    printf("Tuesday");
+    break;
+  case 3:
+    printf("Wednesday");
+    break;
+  case 4:
+    printf("Thursday");
+    break;
+  case 5:
+    printf("Friday");
+    break;
+  case 6:
+    printf("Saturday");
+    break;
+  case 7:
+    printf("Sunday");
+    break;
+  return 0;
+}
