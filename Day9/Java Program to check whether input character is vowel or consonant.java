f337569cf88818e5036936c489360bbb8a5065ee
Ashwin .M <114208625+Ashwin-M-1234@users.noreply.github.com>
2023-07-20 14:50:34 +0530

    Update Java Program to check whether input character is vowel or consonant.java

--- Day9/Java Program to check whether input character is vowel or consonant.java
+++ Day9/Java Program to check whether input character is vowel or consonant.java
@@ -1,1 +1,15 @@
+// to check wheather that the char is vowel or not in java
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char ch = scanner.next().charAt(0);
+        boolean isVowel = "AEIOUaeiou".indexOf(ch) != -1;
+        System.out.println(ch + " is " + (isVowel ? "" : "not ") + "a vowel.");
+        scanner.close();
+    }
+}
 
