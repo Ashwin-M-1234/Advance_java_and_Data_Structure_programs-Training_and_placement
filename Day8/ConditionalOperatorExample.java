1e456cbefe4c2983d7de3e39515d3afbdfb96d39
Ashwin .M <114208625+Ashwin-M-1234@users.noreply.github.com>
2023-07-20 08:40:27 +0530

    Update ConditionalOperatorExample.java

--- Day8/ConditionalOperatorExample.java
+++ Day8/ConditionalOperatorExample.java
@@ -2,8 +2,8 @@ public class ConditionalOperatorExample
 {  
 public static void main(String args[])   
 {  
-int x=5, y=4, z=7;  
-System.out.println(x>y && x>z || y<z);  
-System.out.println((x<z || y>z) && x<y);  
+int x = 5, y = 4, z = 7;
+System.out.println(x>y && x>z || y<z);
+System.out.println((xz || y>z) && x<y);  
 }  
 }  
