e09e9efdd8671904035f072eaf41d0e1350b8a51
Ashwin .M <114208625+Ashwin-M-1234@users.noreply.github.com>
2023-07-20 14:42:00 +0530

    Update Java Program to add two complex numbers.java

--- Day9/Java Program to add two complex numbers.java
+++ Day9/Java Program to add two complex numbers.java
@@ -1,1 +1,24 @@
+// Add two complex numbers in java
+
+class ComplexNum 
+{
+    double real;
+    double imaginary;
+    ComplexNum(double r, double i) 
+    { 
+      real = r; 
+      imaginary = i; 
+    }
+}
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        ComplexNum num1 = new ComplexNum(3.5, 2);
+        ComplexNum num2 = new ComplexNum(1.5, 4.5);
+        ComplexNum sum  = new ComplexNum(num1.real + num2.real, num1.imaginary + num2.imaginary);
+        System.out.println("Sum: " + sum.real + " + " + sum.imaginary + "i");
+    }
+}
 
