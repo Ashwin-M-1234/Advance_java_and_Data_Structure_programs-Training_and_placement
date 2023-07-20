b9603e4da3211aaf049443912c6ae0609b14d11a
Ashwin .M <114208625+Ashwin-M-1234@users.noreply.github.com>
2023-07-20 14:35:24 +0530

    Update Java Program to Multiply two Numbers.java

--- Day9/Java Program to Multiply two Numbers.java
+++ Day9/Java Program to Multiply two Numbers.java
@@ -1,1 +1,16 @@
+//multiply two number in java
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+        double product = num1 * num2;
+        System.out.println("Product: " + product);
+        scanner.close();
+    }
+}
 
