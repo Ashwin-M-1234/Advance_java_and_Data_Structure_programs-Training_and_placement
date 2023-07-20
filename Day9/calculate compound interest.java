e33f070f3a79a9b4d10167a9cde5b84c50ba7e96
Ashwin .M <114208625+Ashwin-M-1234@users.noreply.github.com>
2023-07-20 14:52:38 +0530

    Update calculate compound interest.java

--- Day9/calculate compound interest.java
+++ Day9/calculate compound interest.java
@@ -1,1 +1,21 @@
+// calculate the compound intrest in java
+
+
+import java.util.Scanner;
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the principal amount: ");
+        double principal = scanner.nextDouble();
+        System.out.print("Enter the rate of interest: ");
+        double rate = scanner.nextDouble();
+        System.out.print("Enter the time period (in years): ");
+        double time = scanner.nextDouble();
+        double compoundInterest = (principal * Math.pow(1 + rate / 100, time) - principal);
+        System.out.println("Compound Interest: " + compoundInterest);
+        scanner.close();
+    }
+}
 
