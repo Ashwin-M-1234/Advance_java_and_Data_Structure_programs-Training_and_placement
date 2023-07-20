d2cc890249dc3543fb4ac97aa82ae6c728a278fb
Ashwin .M <114208625+Ashwin-M-1234@users.noreply.github.com>
2023-07-20 23:11:25 +0530

    Create different types of pattern printing.java

---
+++ Day9/different types of pattern printing.java
@@ -0,0 +1,60 @@
+public class SquarePattern {
+    public static void main(String[] args) {
+        int rows = 4;
+        int columns = 4;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+public class RightTrianglePattern {
+    public static void main(String[] args) {
+        int rows = 4;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+public class InvertedRightTrianglePattern {
+    public static void main(String[] args) {
+        int rows = 4;
+
+        for (int i = rows; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+public class PyramidPattern {
+    public static void main(String[] args) {
+        int rows = 4;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
