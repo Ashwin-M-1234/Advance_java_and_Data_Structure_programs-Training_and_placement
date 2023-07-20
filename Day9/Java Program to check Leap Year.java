5aba793711ab24fcbb67ef8119e2ca24fe1b76d7
Ashwin .M <114208625+Ashwin-M-1234@users.noreply.github.com>
2023-07-20 14:44:45 +0530

    Update Java Program to check Leap Year.java

--- Day9/Java Program to check Leap Year.java
+++ Day9/Java Program to check Leap Year.java
@@ -1,1 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+        boolean isLeapYear = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+        System.out.println(year + " is " + (isLeapYear ? "" : "not ") + "a leap year.");
+        scanner.close();
+    }
+}
 
