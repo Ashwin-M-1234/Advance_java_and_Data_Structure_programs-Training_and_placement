2289be18aaa4812cda6f8eb258c962e5710b36b7
Ashwin .M <114208625+Ashwin-M-1234@users.noreply.github.com>
2023-07-20 22:44:42 +0530

    Create ArrayReverse.java

---
+++ Day9/ArrayReverse.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ArrayReverse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        // Reversing the array
+        int start = 0;
+        int end = n - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+
+        System.out.println("The reversed array is:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
