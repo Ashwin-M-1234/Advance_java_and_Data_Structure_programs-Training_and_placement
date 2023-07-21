9423f4300b2a15e75ca980db1022d0b0548792df
Ashwin .M <114208625+Ashwin-M-1234@users.noreply.github.com>
2023-07-21 12:22:24 +0530

    Create Compound Intrest.java

---
+++ Day10/Compound Intrest.java
@@ -0,0 +1,33 @@
+ 
+// to calculate the compound interest
+import java.util.Scanner;
+public class CompoundIntrest {
+  public static void main(String[] args) {
+
+    // create an object of Scanner class
+    Scanner sc = new Scanner (System.in);
+
+    // take input from users
+    System.out.print("Enter the principal: ");
+    double p = sc.nextDouble();
+
+    System.out.print("Enter the rate: ");
+    double r = sc.nextDouble();
+
+    System.out.print("Enter the time: ");
+    double t = sc.nextDouble();
+
+    System.out.print("Enter number of times interest is compounded: ");
+    int n = sc.nextInt();
+
+    double A = p * (Math.pow((1 + r/100), (t*n))) - p;
+
+    System.out.println("Principal: " + p);
+    System.out.println("Interest Rate: " + r);
+    System.out.println("Time Duration: " + t);
+    System.out.println("Number of Time interest Compounded: " + n);
+    System.out.println("Compound Interest: " + i);
+
+    sc.close();
+  }
+}
